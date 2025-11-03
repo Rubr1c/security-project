53c294d2ee5c4561a4cf2bb77b81638b8d3d42bd
Rubr1c <alizaghloul64@gmail.com>
2025-11-03 19:40:59 +0200

    add packages + setup prettier

---
+++ .prettierrc.js
@@ -0,0 +1,26 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'auto',
+  singleAttributePerLine: false,
+  plugins: ['prettier-plugin-tailwindcss'],
+};
+
+module.exports = config;
